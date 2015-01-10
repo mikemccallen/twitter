9cdf2f9a00f0076258247291e93c14b4bd087cee
Mike Mccallen <mikemccallen1@gmail.com>
2015-01-10 20:48:08 +0000

    Add sign in files

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
-  def home
+  def create
+    Person.create(person_params)
   end
 
-  def action
+  private
+
+  def person_params
+    params.require(:person).permit(:name, :age)
   end
 end
