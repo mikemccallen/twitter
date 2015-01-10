b3d1eff91b36b70f5b2399f51db943cf3bf0b533
Mike Mccallen <mikemccallen1@gmail.com>
2015-01-10 20:58:07 +0000

    Add sigin in files

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module Twitter
 
 # Required for devide on Heroku
      config.assets.initialize_on_precompile = false
-     config.active_record.whitelist_attributes = true 
+     config.active_record.whitelist_attributes = false
 
   end
 end
