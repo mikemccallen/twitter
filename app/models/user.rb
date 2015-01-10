e156f40b3145810c04b6aa219e3f18f045729b25
Mike Mccallen <mikemccallen1@gmail.com>
2015-01-10 21:49:38 +0000

    Sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
- include ActiveModel::ForbiddenAttributesProtection
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
