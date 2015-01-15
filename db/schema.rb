3b8bc4245507dbaaebbb5cc70febcedbfe48fec7
Mike Mccallen <mikemccallen1@gmail.com>
2015-01-15 20:50:51 +0000

    Fixing the name field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150113200656) do
+ActiveRecord::Schema.define(version: 20150115200022) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150113200656) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "part_number"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
