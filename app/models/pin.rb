86e3791337859669e3ee09daf05a2f81655f45ba
Mike Mccallen <mikemccallen1@gmail.com>
2015-01-19 20:10:06 +0000

    Fix

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,11 +1,8 @@
 class Pin < ActiveRecord::Base
-has_attached_file :image, :styles => { :medium => "700x800>" },
-								  :url  => "/assets/products/:id/:style/:basename.:extension",
-                  :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
 
 	validates :description, presence: true
 	validates :user_id, presence: true
-	has_attached_file :image
+	has_attached_file :image, styles: { medium: "500x500>", small: "200x200>"}
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 
