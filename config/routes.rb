316b4683dbddfae6a8eb8cb0ff02e5d4a9136729
Mike Mccallen <mikemccallen1@gmail.com>
2015-01-22 17:46:00 +0000

    Added jQuery masonry

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   get 'about' => 'pages#about'
-root 'pages#home'
+root 'pins#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
