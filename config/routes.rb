4ecb413e5372e1816d5ecd3306671ecfbfc187f6
Mike Mccallen <mikemccallen1@gmail.com>
2015-01-24 07:42:13 +0000

    Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :pins
+
+    get 'about' => 'pages#about'
+root 'pins#index'
 
   devise_for :users
-  get 'about' => 'pages#about'
-root 'pins#index'
+  resources :pins
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
