b3b6e6620410a12ecba7508fcff40280e9263764
Mike Mccallen <mikemccallen1@gmail.com>
2015-01-24 07:17:24 +0000

    Commi1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,5 +42,4 @@ Rails.application.configure do
     # In production, :host should be set to the actual host of your application.
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   Paperclip.options[:command_path] = 'C:\ImageMagicks'
-  config.assets.raise_production_errors = true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-
-    get 'about' => 'pages#about'
-root 'pins#index'
-
-  devise_for :users
   resources :pins
 
+  devise_for :users
+  get 'about' => 'pages#about'
+root 'pins#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
